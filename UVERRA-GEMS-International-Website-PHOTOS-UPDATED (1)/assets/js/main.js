f97bff4ce24d9fed34398140369650f07a554ee3
uverragems-blip <uverragems@gmail.com>
2026-09-12 01:42:14 +0530

    ch

---
+++ UVERRA-GEMS-International-Website-PHOTOS-UPDATED (1)/assets/js/main.js
@@ -0,0 +1,1 @@
+const menu=document.querySelector('.menu-btn');const nav=document.querySelector('#mainNav');if(menu){menu.addEventListener('click',()=>nav.classList.toggle('open'));nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')))}
